e0c0b14af55b0117fa58c8e4a757094a43396bb7
balasitheaswaranb <106142619+balasitheaswaranb@users.noreply.github.com>
2022-11-15 19:41:36 +0530

    Add files via upload

--- Mentor_assignments/Beautiful.java
+++ Mentor_assignments/Beautiful.java
@@ -1,6 +1,7 @@
 package com.dailylearning;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Beautiful {
 	public static void main(String args[]) {
@@ -8,35 +9,37 @@ public class Beautiful {
 	}
 
 	private void init() {
-		String s = "ivvkx";
-		char sChar[] = s.toCharArray();
-		int[] sInt = new int[sChar.length];
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		char inputArray[] = input.toCharArray();
+		int[] intArray1 = new int[inputArray.length];
 
-		StringBuilder s1 = new StringBuilder(s);
-		s1.reverse();
-		String r = s1.toString();
-		char sChar1[] = r.toCharArray();
-		int[] sInt1 = new int[sChar1.length];
+		StringBuilder stringBuilder = new StringBuilder(input);
+		stringBuilder.reverse();
+		String reversedString = stringBuilder.toString();
+		char reversedArray[] = reversedString.toCharArray();
+		int[] intArray2 = new int[reversedArray.length];
 
-		for (int i = 0; i < sChar.length; i++) {
-			sInt[i] = sChar[i];
+		for (int i = 0; i < inputArray.length; i++) {
+			intArray1[i] = inputArray[i];
 		}
-		for (int i = 0; i < sChar1.length; i++) {
-			sInt1[i] = sChar1[i];
+		for (int i = 0; i < reversedArray.length; i++) {
+			intArray2[i] = reversedArray[i];
 		}
-		int count[] = new int[sInt.length - 1];
-		for (int i = 0; i < sInt.length - 1; i++) {
-			count[i] = sInt[i + 1] - sInt[i];
+		int count1[] = new int[intArray1.length - 1];
+		for (int i = 0; i < intArray1.length - 1; i++) {
+			count1[i] = intArray1[i + 1] - intArray1[i];
 		}
-		int count1[] = new int[sInt1.length - 1];
-		for (int i = 0; i < sInt1.length - 1; i++) {
-			count1[i] = sInt1[i] - sInt1[i + 1];
+		int count2[] = new int[intArray2.length - 1];
+		for (int i = 0; i < intArray2.length - 1; i++) {
+			count2[i] = intArray2[i] - intArray2[i + 1];
 		}
-		Arrays.sort(count);
 		Arrays.sort(count1);
-		if (Arrays.equals(count, count1)) {
+		Arrays.sort(count2);
+		if (Arrays.equals(count1, count2)) {
 			System.out.println("Yes");
 		} else
 			System.out.println("no");
+		scanner.close();
 	}
 }
